f5c129625a247b058b905443a0454e9d9b0a519c
djumanov <djumanovdev@gmail.com>
2026-02-03 09:48:51 +0500

    task

--- api/models.py
+++ api/models.py
@@ -8,3 +8,17 @@ class Item(models.Model):
     price = models.FloatField()
     is_active = models.BooleanField()
 
+
+class Product(models.Model):
+    name = models.CharField()
+    desc = models.TextField()
+    category = models.CharField()
+    price = models.FloatField()
+    is_active = models.BooleanField()
+    stock = models.PositiveIntegerField()
+
+# GET api/products
+# POST api/products
+# GET api/products/id
+# PUT api/products/id
+# DELETE api/products/id
